6a907d3570d5b9ada4118935d5e432594282c619
zaidanalbert <m006d4ky2194@bangkit.academy>
2024-03-05 16:10:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -130,24 +130,22 @@ plt.xticks(fontsize=12)
 st.pyplot(fig)
 
 # Customer Demographic
+
 st.subheader("Customer Demographic")
-tab1 = st.tabs(["State"])
+most_common_state = state.customer_state.value_counts().index[0]
+st.markdown(f"Most Common State: **{most_common_state}**")
 
-with tab1:
-    most_common_state = state.customer_state.value_counts().index[0]
-    st.markdown(f"Most Common State: **{most_common_state}**")
+fig, ax = plt.subplots(figsize=(12, 6))
+sns.barplot(x=state.customer_state.value_counts().index,
+            y=state.customer_count.values, 
+            data=state,
+            palette=["#90CAF9" if score == most_common_state else "#D3D3D3" for score in state.customer_state.value_counts().index]
+                )
 
-    fig, ax = plt.subplots(figsize=(12, 6))
-    sns.barplot(x=state.customer_state.value_counts().index,
-                y=state.customer_count.values, 
-                data=state,
-                palette=["#90CAF9" if score == most_common_state else "#D3D3D3" for score in state.customer_state.value_counts().index]
-                    )
-
-    plt.title("Number customers from State", fontsize=15)
-    plt.xlabel("State")
-    plt.ylabel("Number of Customers")
-    plt.xticks(fontsize=12)
-    st.pyplot(fig)
+plt.title("Number customers from State", fontsize=15)
+plt.xlabel("State")
+plt.ylabel("Number of Customers")
+plt.xticks(fontsize=12)
+st.pyplot(fig)
 
 st.caption('Copyright (C) M. Zaidan A. 2024')
